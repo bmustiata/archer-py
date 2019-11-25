52930398d22a1a60049c068689e9123de19c4843
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-11-25 22:25:47 +0100

    List alphabetically + colors.

--- project_archer/operations/list_projects.py
+++ project_archer/operations/list_projects.py
@@ -1,4 +1,5 @@
 import os
+from termcolor_util import cyan
 
 from project_archer.environment.read_shell_parameters import project_folder
 from project_archer.storage.project_data import read_project_yml
@@ -8,10 +9,12 @@ def list_projects(args, env):
 
     env.log("Available projects:")
 
-    for filename in os.listdir(folder):
+    items = list(sorted(os.listdir(folder)))
+
+    for filename in items:
         if not os.path.isfile(os.path.join(folder, filename)):
             continue
 
         file_data = open(os.path.join(folder, filename))
         project_data = read_project_yml(file_data)
-        env.log(" - " + filename + ": " + project_data['name'])
+        env.log(" - " + cyan(os.path.splitext(filename)[0], bold=True) + ": " + project_data['name'])
