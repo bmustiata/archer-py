2f1d876928a43540879ec0cdebf3e6b9238779ac
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-12-03 03:18:20 +0100

    Minor cleanups.

--- project_archer/storage/project_data.py
+++ project_archer/storage/project_data.py
@@ -3,23 +3,26 @@ import yaml
 
 def read_project_yml(data):
     loaded_data = yaml.safe_load(data)
-    project = loaded_data.get('config') or loaded_data.get('layout')
-    if not project:
-        project = {'name': "<broken data>"}
+    try:
+        project = loaded_data.get('config') or loaded_data.get('layout')
+        if not project:
+            project = {'name': "<broken data>"}
 
-    ensure_array(project, "requires")
-    ensure_array(project, "layouts")
-    ensure_array(project, "activate")
-    ensure_array(project, "deactivate")
+        ensure_array(project, "requires")
+        ensure_array(project, "layouts")
+        ensure_array(project, "activate")
+        ensure_array(project, "deactivate")
 
-    if 'exports' not in project:
-        project['exports'] = {}
-    if 'commands' not in project:
-        project['commands'] = {}
-    if 'name' not in project:
-        project['name'] = "<not defined>"
+        if 'exports' not in project:
+            project['exports'] = {}
+        if 'commands' not in project:
+            project['commands'] = {}
+        if 'name' not in project:
+            project['name'] = "<not defined>"
 
-    return project
+        return project
+    except Exception as e:
+        raise Exception("Unable to read loaded data: %s" % loaded_data, e)
 
 
 def ensure_array(obj, property_name):
