4d0dc7bc57c441f86c4dc686130ac87644278f03
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-09-30 03:52:41 +0200

    Updated requires

--- setup.py
+++ setup.py
@@ -8,14 +8,17 @@ packages = find_packages()
 
 setup(
     name="project_archer",
-    version="0.1.4",
+    version="0.3.0",
     description="Switch projects with ease.",
     long_description=readme,
     author="Bogdan Mustiata",
     author_email="bogdan.mustiata@gmail.com",
     license="BSD",
     entry_points={"console_scripts": ["archer = project_archer.mainapp:main"]},
-    install_requires=["termcolor-util"],
+    install_requires=[
+        "termcolor-util",
+        "click",
+        "PyYAML >=5.1, <5.2"],
     packages=packages,
     package_data={"": ["*.txt", "*.rst"]},
 )
